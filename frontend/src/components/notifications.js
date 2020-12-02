279246f30447f255ae33e08164b9384cafd400b6
sachinvivek <55019801+sachinvivek@users.noreply.github.com>
2020-12-02 12:38:01 +0530

    Add files via upload

--- frontend/src/components/notifications.js
+++ frontend/src/components/notifications.js
@@ -1,13 +1,48 @@
 import React from 'react'
 import './bookings.css'
-function Notifications(){
-    return(
-        <body>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"></link>
-            <div>
-                <div className='justify-content-lg-center'>NOTIFICATIONS</div>
-            </div>
-        </body>
-    );
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+      minWidth: 100,
+      width:'50%',
+      marginLeft: '25%',
+    },
+    title: {
+      fontSize: 20,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  });
+  
+export default function OutlinedCard() {
+  const classes = useStyles();
+    return (
+    <body>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"></link>
+    <div>
+        <div className='justify-content-lg-center'>NOTIFICATIONS</div>
+    </div>
+    <Card className={classes.root} variant="outlined">
+      <CardContent>
+        <Typography variant="h4" >
+          Visitor Alert!
+        </Typography>
+        <Typography variant="h5" component="p">
+          Name
+        </Typography>
+        <Typography variant="h5" component="p">
+          Purpose
+        </Typography>
+        <Typography variant="h5" component="p">
+          Phone Number
+        </Typography>
+      </CardContent>
+          </Card>
+    </body>
+  );
 }
-export default Notifications;
