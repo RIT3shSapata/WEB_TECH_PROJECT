67c50fb5516cefa984cc0c97bd1b958a5b156f98
sahanashettigar <r.sahana.shettigar@gmail.com>
2020-12-02 18:18:29 +0530

    navbar update done

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -21,11 +21,13 @@ function Navbar(){
         <link rel="preconnect" href="https://fonts.gstatic.com"></link>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap" rel="stylesheet"></link>
   <div class='navbar'>
-    <div class='home' onClick={homePage}><HomeIcon className={"homebutton"}></HomeIcon>  APNA GHAR</div>
+    <div className="flex1"><div class='home' onClick={homePage}><HomeIcon className={"homebutton"}></HomeIcon>  APNA GHAR</div></div>
+    <div className="flex2">
     <div class='notifications'><Link to='./notifications'><AccessAlarmsIcon className={"notificationsbutton"} ></AccessAlarmsIcon></Link></div>
     <div class='bookings'><Link to='./bookings'><EventIcon className={"notificationsbutton"} ></EventIcon></Link></div>
     <div class='announcement'><Link to='./announcement'><AnnouncementIcon className={"notificationsbutton"} ></AnnouncementIcon></Link></div>
     <div class='signout' onClick={loginPage}>SIGN OUT</div>
+    </div>
   </div>
 </body>
     );
