f7c607b861e1ddbe7a2d0ffb1b806dca50fdd98a
Ritesh Sapata <riteshsapata@gmail.com>
2020-12-04 20:12:48 +0530

    user data added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
+import reducers from './reducers';
 
+const store = createStore(reducers, applyMiddleware(thunk));
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
