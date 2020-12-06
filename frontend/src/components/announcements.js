9a4dbd770f66ce38417a493cd247494f6f1f7639
sachinvivek <55019801+sachinvivek@users.noreply.github.com>
2020-12-06 10:43:14 +0530

    Add files via upload

--- frontend/src/components/announcements.js
+++ frontend/src/components/announcements.js
@@ -1,33 +1,209 @@
 import React from 'react'
 import './announcements.css';
-function Announcements(){
-    return(
+import { Container, Card } from 'react-bootstrap'
+function Announcements() {
+    return (
         <body>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"></link>
-            <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-            <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap" rel="stylesheet"></link>
+                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"></link>
+                <link rel="preconnect" href="https://fonts.gstatic.com"></link>
             <div>
                 <div className='justify-content-lg-center'>ANNOUNCEMENTS</div>
-                <div>
-            <div className='column'>
-            </div>
-            <div className='column'>
-            </div>
-            <div className='column'>
-            </div>
-            <div className='column'>
-            </div>
-            <div className='column'>
-            </div>
-            <div className='column'>
-            </div>
-            <div className='column'>
-            </div>
-            <div className='column'>
-            </div>
-        </div>
-            </div>
-        </body>
+                </div>
+                <Container>
+                    <div className="mt-1">
+                    <Card style={{ width: '80%' }}>
+                    <div className="description">
+                        <Card.Header>Title</Card.Header>
+                        </div>
+                        <Card.Body> 
+                            <div className="description">
+                <Card.Text>
+                 Description
+                </Card.Text>
+                </div>
+                <div className="date">
+                <Card.Text>
+                 Time and Date
+                </Card.Text>
+                </div>
+                </Card.Body>
+                    </Card>
+                    </div>
+                    <div className="mt-3">
+                    <Card style={{ width: '80%' }}>
+                    <div className="description">
+                        <Card.Header>Title</Card.Header>
+                        </div>
+                        <Card.Body> 
+                            <div className="description">
+                <Card.Text>
+                 Description
+                </Card.Text>
+                </div>
+                <div className="date">
+                <Card.Text>
+                 Time and Date
+                </Card.Text>
+                </div>
+                </Card.Body>
+                    </Card>
+                    </div>
+                    <div className="mt-3">
+                    <Card style={{ width: '80%' }}>
+                    <div className="description">
+                        <Card.Header>Title</Card.Header>
+                        </div>
+                        <Card.Body> 
+                            <div className="description">
+                <Card.Text>
+                 Description
+                </Card.Text>
+                </div>
+                <div className="date">
+                <Card.Text>
+                 Time and Date
+                </Card.Text>
+                </div>
+                </Card.Body>
+                    </Card>
+                    </div>
+                    <div className="mt-3">
+                    <Card style={{ width: '80%' }}>
+                    <div className="description">
+                        <Card.Header>Title</Card.Header>
+                        </div>
+                        <Card.Body> 
+                            <div className="description">
+                <Card.Text>
+                 Description
+                </Card.Text>
+                </div>
+                <div className="date">
+                <Card.Text>
+                 Time and Date
+                </Card.Text>
+                </div>
+                </Card.Body>
+                    </Card>
+                    </div>
+                    <div className="mt-3">
+                    <Card style={{ width: '80%' }}>
+                    <div className="description">
+                        <Card.Header>Title</Card.Header>
+                        </div>
+                        <Card.Body> 
+                            <div className="description">
+                <Card.Text>
+                 Description
+                </Card.Text>
+                </div>
+                <div className="date">
+                <Card.Text>
+                 Time and Date
+                </Card.Text>
+                </div>
+                </Card.Body>
+                    </Card>
+                    </div>
+                    <div className="mt-3">
+                    <Card style={{ width: '80%' }}>
+                    <div className="description">
+                        <Card.Header>Title</Card.Header>
+                        </div>
+                        <Card.Body> 
+                            <div className="description">
+                <Card.Text>
+                 Description
+                </Card.Text>
+                </div>
+                <div className="date">
+                <Card.Text>
+                 Time and Date
+                </Card.Text>
+                </div>
+                </Card.Body>
+                    </Card>
+                    </div>
+                    <div className="mt-3">
+                    <Card style={{ width: '80%' }}>
+                    <div className="description">
+                        <Card.Header>Title</Card.Header>
+                        </div>
+                        <Card.Body> 
+                            <div className="description">
+                <Card.Text>
+                 Description
+                </Card.Text>
+                </div>
+                <div className="date">
+                <Card.Text>
+                 Time and Date
+                </Card.Text>
+                </div>
+                </Card.Body>
+                    </Card>
+                    </div>
+                    <div className="mt-3">
+                    <Card style={{ width: '80%' }}>
+                    <div className="description">
+                        <Card.Header>Title</Card.Header>
+                        </div>
+                        <Card.Body> 
+                            <div className="description">
+                <Card.Text>
+                 Description
+                </Card.Text>
+                </div>
+                <div className="date">
+                <Card.Text>
+                 Time and Date
+                </Card.Text>
+                </div>
+                </Card.Body>
+                    </Card>
+                    </div>
+                    <div className="mt-3">
+                    <Card style={{ width: '80%' }}>
+                    <div className="description">
+                        <Card.Header>Title</Card.Header>
+                        </div>
+                        <Card.Body> 
+                            <div className="description">
+                <Card.Text>
+                 Description
+                </Card.Text>
+                </div>
+                <div className="date">
+                <Card.Text>
+                 Time and Date
+                </Card.Text>
+                </div>
+                </Card.Body>
+                    </Card>
+                    </div>
+                    <div className="mt-3">
+                    <Card style={{ width: '80%' }}>
+                    <div className="description">
+                        <Card.Header>Title</Card.Header>
+                        </div>
+                        <Card.Body> 
+                            <div className="description">
+                <Card.Text>
+                 Description
+                </Card.Text>
+                </div>
+                <div className="date">
+                <Card.Text>
+                 Time and Date
+                </Card.Text>
+                </div>
+                </Card.Body>
+                    </Card>
+                    </div>  
+                </Container>
+                </body>
     );
 }
 export default Announcements;
+
+                   
